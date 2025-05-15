9fbb7d8f33e7397573a363608a86625be68cb912
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2025-05-15 17:31:39 +0530

    loader file added

---
+++ src/component/Loader.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Loader: React.FC = () => {
+  return (
+    <div className="fixed inset-0 backdrop-brightness-60 flex items-center justify-center z-50 bg-opacity-50">
+      <div className="bg-gray-800 h-[130px] w-[300px] gap-y-3 bg-opacity-90 rounded-lg flex flex-col items-center justify-center px-6">
+        <div className="relative">
+          <div
+            className="w-10 h-10 rounded-full animate-spin-custom"
+            style={{
+              background: 'conic-gradient(from 0deg, rgba(156, 163, 175, 1) 0%, rgba(156, 163, 175, 0.1) 100%)',
+              clipPath: 'circle(50% at 50% 50%)', // Outer circle
+              WebkitMask: 'radial-gradient(circle at 50% 50%, transparent 50%, black 60%)', // Hollow center, thicker ring
+            }}
+          ></div>
+        </div>
+        <div className="text-white text-lg font-semibold">Loading...</div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
