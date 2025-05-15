4e4fa1b098be2a1ff13dc539acd4395061d0ad56
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2025-05-15 10:51:57 +0530

    font added

--- src/pages/CanteenSurvay.tsx
+++ src/pages/CanteenSurvay.tsx
@@ -22,14 +22,14 @@ const CanteenSurvay = () => {
     <div className="w-full  xl:w-[96%] mx-1 p-4">
     <div className="mb-6">
       <h1 className="text-[16px] font-[700]">ENCOMPASS BRKSVILL 941562</h1>
-      <p>16680 BALANCE COVE</p>
+      <p className='text-[16px] font-[700]'>16680 BALANCE COVE</p>
       <p>16680 BALANCE COVE</p>
       <p>Land O Lakes, FL 34638</p>
-      <p >Snacks</p>
+      <p className='text-[16px] font-[700]'>Snacks</p>
       <Link
           to="/"
           onClick={handleOpenModal}
-          className="text-[#c1f001] underline hover:text-[#005599]"
+          className="text-[#c1f001] text-[16px] font-[700] underline hover:text-[#005599]"
         >
           Not at this location?
         </Link>
@@ -39,7 +39,7 @@ const CanteenSurvay = () => {
       <div className="bg-white rounded-[11px] overflow-hidden border border-black">
         <Link
           to="/machine-problem"
-          className="w-full px-3 py-2 text-left flex justify-between items-center link-item"
+          className="w-full px-3 py-3 text-left flex justify-between items-center link-item"
         >
           <span className="font-[700] text-[16px]">Machine Problem?</span>
           <FaChevronCircleRight className='text-[22px] text-[#4D4D4D] '/>
@@ -47,7 +47,7 @@ const CanteenSurvay = () => {
         <div className="border-t border-black"></div>
         <Link
           to="/refunds"
-          className="w-full px-3 py-2 text-left flex justify-between items-center link-item"
+          className="w-full px-3 py-3 text-left flex justify-between items-center link-item"
         >
           <span className="font-[700] text-[16px]">Need a Refund?</span>
           <FaChevronCircleRight className='text-[22px] text-[#4D4D4D] '/>
@@ -55,7 +55,7 @@ const CanteenSurvay = () => {
         <div className="border-t border-black"></div>
         <Link
           to="/nutrition"
-          className="w-full px-3 py-2 text-left flex justify-between items-center link-item"
+          className="w-full px-3 py-3 text-left flex justify-between items-center link-item"
         >
           <span className="font-[700] text-[16px]">Nutrition Information</span>
           <FaChevronCircleRight className='text-[22px] text-[#4D4D4D] '/>
