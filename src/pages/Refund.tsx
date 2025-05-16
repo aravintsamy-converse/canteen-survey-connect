56541c0e0952fa08eb47f3104289191237e37cc6
aravintsamy-converse <aravintsamy@conversedatasolutions.com>
2025-05-16 15:34:35 +0530

    added dropdown

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -7,9 +7,19 @@ const Refund = () => {
   const { eqpId } = useEqpId();
 
   // Check if eqpId is null, undefined, or empty
-  // if (!eqpId || eqpId === '') {
-  //   return <Navigate to="/survey/not-found" replace />;
-  // }
+  if (!eqpId || eqpId === '') {
+    return <Navigate to="/survey/not-found" replace />;
+  }
+
+  const refundReasonOptions = [
+    { value: "", label: "Choose one" },
+    { value: "otherIssue", label: "Other issue" },
+    { value: "didntReceiveProduct", label: "Didn't receive product" },
+    { value: "moneyJammed", label: "My money jammed" },
+    { value: "pastSellByDate", label: "Past sell by date" },
+    { value: "productDamaged", label: "Product is damaged" },
+    { value: "didntReceiveChange", label: "Didn't receive change" }
+  ];
 
   const [formData, setFormData] = useState({
     eqpId: eqpId,
@@ -18,21 +28,14 @@ const Refund = () => {
     email: '',
     phone: '',
     comments: '',
-    issues: {
-      needsToBeFilled: false,
-      machineIsNotWorking: false,
-      wontAcceptCreditCard: false,
-      machineIsDamaged: false,
-      needsCleaning: false,
-      notAcceptingMoney: false,
-    }
+    selectedReason: ''
   });
 
   const [errors, setErrors] = useState({
     refund_amount: '',
     name: '',
     email: '',
-    issues: '',
+    selectedReason: '',
     comments: ''
   });
 
@@ -40,14 +43,16 @@ const Refund = () => {
     refund_amount: false,
     name: false,
     email: false,
-    comments: false
+    comments: false,
+    selectedReason: false
   });
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [commentCharsRemaining, setCommentCharsRemaining] = useState(500);
   const [nameCharsRemaining, setNameCharsRemaining] = useState(50);
   const [isFormDirty, setIsFormDirty] = useState(false);
 
-  usePrompt(isFormDirty, 'This survey must be completed or all your results will be lost.\n Do you still wish to exit?');
+  usePrompt(isFormDirty, 'This survey must be completed or all your results will be lost.\nDo you still wish to exit?');
 
   // Check if form is dirty (i.e., has been modified)
   const checkFormDirty = () => {
@@ -57,7 +62,7 @@ const Refund = () => {
       formData.email !== '' ||
       formData.phone !== '' ||
       formData.comments !== '' ||
-      Object.values(formData.issues).some(value => value)
+      formData.selectedReason !== ''
     );
   };
 
@@ -66,14 +71,19 @@ const Refund = () => {
     setIsFormDirty(checkFormDirty());
   }, [formData]);
 
-  const handleIssueChange = (issue: keyof typeof formData.issues) => {
+  const handleSelecReason = (value: string) => {
     setFormData({
       ...formData,
-      issues: {
-        ...formData.issues,
-        [issue]: !formData.issues[issue]
-      }
+      selectedReason: value
     });
+
+    setTouched(prev => ({
+      ...prev,
+      selectedReason: true
+    }));
+
+    validateField('selectedReason', value);
+    setDropdownOpen(false);
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -96,7 +106,7 @@ const Refund = () => {
       [name]: true
     }));
 
-    validateField(name, value);
+    validateField(name as keyof typeof errors, value as string);;
   };
 
   useEffect(() => {
@@ -128,6 +138,10 @@ const Refund = () => {
       if (!value.trim()) {
         errorMessage = '* Required';
       }
+    } else if (name === 'selectedReason') {
+      if (!value) {
+        errorMessage = '* Required';
+      }
     }
 
     setErrors(prev => ({
@@ -145,7 +159,7 @@ const Refund = () => {
       refund_amount: '',
       name: '',
       email: '',
-      issues: '',
+      selectedReason: '',
       comments: ''
     };
 
@@ -175,9 +189,8 @@ const Refund = () => {
       }
     }
 
-    const hasSelectedIssue = Object.values(formData.issues).some(value => value);
-    if (!hasSelectedIssue) {
-      newErrors.issues = 'Please select at least one issue';
+    if (!formData.selectedReason) {
+      newErrors.selectedReason = 'Please select an reason';
       isValid = false;
     }
 
@@ -194,67 +207,72 @@ const Refund = () => {
     }
   };
 
+  const getSelectedLabel = () => {
+    const selected = refundReasonOptions.find(option => option.value === formData.selectedReason);
+    return selected ? selected.label : "Please select a reason for a refund";
+  };
+
   return (
-    <div className="w-full xl:w-[96%]  px-3 py-5 min-h-screen">
-      <h1 className="text-[22px] font-[700] text-white">Machine Problem? </h1>
-      <div className="pt-[58px] mx-auto">
+    <div className="w-full  px-3 py-5 min-h-screen">
+      <h1 className="text-2xl font-bold text-white">Need a Refund?</h1>
+      <div className="pt-14 mx-auto">
         <div className="mb-6">
-          <p className="font-[400] text-[16px] mb-0" style={{ textShadow: '0 0 0 #444444' }}>Choose all issues that apply:</p>
-          <div className="border border-[#000] bg-white overflow-hidden rounded-[12px] w-full md:w-[78%]">
-            {Object.entries(formData.issues).map(([key, value], index) => {
-              const issueText = (() => {
-                switch (key) {
-                  case 'needsToBeFilled':
-                    return 'Needs to be filled';
-                  case 'machineIsNotWorking':
-                    return 'Machine is not working';
-                  case 'wontAcceptCreditCard':
-                    return 'Won\'t accept credit card';
-                  case 'machineIsDamaged':
-                    return 'Machine is damaged';
-                  case 'needsCleaning':
-                    return 'Needs cleaning';
-                  case 'notAcceptingMoney':
-                    return 'Not Accepting Money';
-                  default:
-                    return '';
-                };
-              })();
-              return (
-                <div
-                  key={key}
-                  className={`p-3 flex items-center link-item border-[#000] h-11 ${index !== Object.entries(formData.issues).length - 1 ? 'border-b' : ''}`}
-                  onClick={() => handleIssueChange(key as keyof typeof formData.issues)}
-                >
-                  <input
-                    type="checkbox"
-                    id={key}
-                    checked={value}
-                    onChange={() => handleIssueChange(key as keyof typeof formData.issues)}
-                    className="mr-3 h-5 w-5 custom-checkbox"
-                  />
-                  <label
-                    htmlFor={key}
-                    onClick={(e) => e.stopPropagation()}
-                    className="select-none text-black text-[16px] font-[700]"
-                    style={{ textShadow: '0 0 0 #444444' }}
-                  >
-                    {issueText}
-                  </label>
-                </div>
-              );
-            })}
+          <label htmlFor="refund_amount" className="block mb-1 text-base font-normal" style={{ textShadow: '0 0 0 #444444' }}>
+          Please select a reason for a refund:{errors.selectedReason && <span className="text-red-500">{errors.selectedReason}</span>}
+          </label>          
+          <div className="relative">
+            <div
+              className="flex justify-between items-center py-2 px-4 bg-lime-600 text-white rounded-t cursor-pointer"
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              <span>{getSelectedLabel()}</span>
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+                />
+              </svg>
+            </div>
+
+            {dropdownOpen && (
+              <div className="absolute z-10 w-full bg-white border border-gray-800 rounded-b">
+                {refundReasonOptions.map((option) => (
+                  option.value && (
+                    <div
+                      key={option.value}
+                      className={`p-3 hover:bg-gray-100 cursor-pointer border-t border-gray-800 ${formData.selectedReason === option.value ? 'bg-blue-100' : ''
+                        }`}
+                      onClick={() => handleSelecReason(option.value)}
+                    >
+                      <span className={`${formData.selectedReason === option.value ? 'font-bold' : ''}`}>
+                        {option.label}
+                      </span>
+                    </div>
+                  )
+                ))}
+              </div>
+            )}
           </div>
-          {errors.issues && (
-            <p className="text-red-500 mt-1">{errors.issues}</p>
+          {errors.selectedReason && touched.selectedReason && (
+            <p className="text-red-500 mt-1">{errors.selectedReason}</p>
           )}
         </div>
 
-
         <div className="mb-6">
-          <p className="mb-2 text-[16px] font-[700]" style={{ textShadow: '0 0 0 #444444' }}>If you would like to receive a confirmation email please provide contact information below:</p>
+          <p className="mb-2 text-base font-bold" style={{ textShadow: '0 0 0 #444444' }}>
+            If you would like to receive a confirmation email please provide contact information below:
+          </p>
+
           <div className="mb-4">
-            <label htmlFor="name" className="block mb-1 text-[16px] font-[400]" style={{ textShadow: '0 0 0 #444444' }}>
+            <label htmlFor="refund_amount" className="block mb-1 text-base font-normal" style={{ textShadow: '0 0 0 #444444' }}>
               Refund amount: {errors.refund_amount && <span className="text-red-500">{errors.refund_amount}</span>}
             </label>
             <input
@@ -264,12 +282,12 @@ const Refund = () => {
               value={formData.refund_amount}
               onChange={handleInputChange}
               onBlur={handleBlur}
-              maxLength={50}
-              className="w-full p-2 border border-[#464646] rounded-[12px] bg-[#808080] focus:outline-none focus:ring-0 focus:ring-[#464646] focus:shadow-[0_0_12px_#92ae1f]"
+              className="w-full p-2 border border-gray-600 rounded-xl bg-gray-500 focus:outline-none focus:ring-0 focus:shadow-lg"
             />
           </div>
+
           <div className="mb-4">
-            <label htmlFor="name" className="block mb-1 text-[16px] font-[400]" style={{ textShadow: '0 0 0 #444444' }}>
+            <label htmlFor="name" className="block mb-1 text-base font-normal" style={{ textShadow: '0 0 0 #444444' }}>
               Name: {errors.name && <span className="text-red-500">{errors.name}</span>}
             </label>
             <input
@@ -280,15 +298,15 @@ const Refund = () => {
               onChange={handleInputChange}
               onBlur={handleBlur}
               maxLength={50}
-              className="w-full p-2 border border-[#464646] rounded-[12px] bg-[#808080] focus:outline-none focus:ring-0 focus:ring-[#464646] focus:shadow-[0_0_12px_#92ae1f]"
+              className="w-full p-2 border border-gray-600 rounded-xl bg-gray-500 focus:outline-none focus:ring-0 focus:shadow-lg"
             />
-            <div className="text-left text-[16px] mt-1 font-[400]" style={{ textShadow: '0 0 0 #444444' }}>
+            <div className="text-left text-base mt-1 font-normal" style={{ textShadow: '0 0 0 #444444' }}>
               {nameCharsRemaining} Characters Remaining
             </div>
           </div>
 
           <div className="mb-4">
-            <label htmlFor="email" className="block mb-1 text-[16px] font-[400]" style={{ textShadow: '0 0 0 #444444' }}>
+            <label htmlFor="email" className="block mb-1 text-base font-normal" style={{ textShadow: '0 0 0 #444444' }}>
               Email Address: {errors.email && <span className="text-red-500">{errors.email}</span>}
             </label>
             <input
@@ -298,12 +316,12 @@ const Refund = () => {
               value={formData.email}
               onChange={handleInputChange}
               onBlur={handleBlur}
-              className="w-full p-2 border border-[#464646] rounded-[12px] bg-[#808080] focus:outline-none focus:ring-0 focus:ring-[#464646] focus:shadow-[0_0_12px_#92ae1f]"
+              className="w-full p-2 border border-gray-600 rounded-xl bg-gray-500 focus:outline-none focus:ring-0 focus:shadow-lg"
             />
           </div>
 
           <div className="mb-4">
-            <label htmlFor="phone" className="block mb-1 text-[16px] font-[400]" style={{ textShadow: '0 0 0 #444444' }}>
+            <label htmlFor="phone" className="block mb-1 text-base font-normal" style={{ textShadow: '0 0 0 #444444' }}>
               Phone (e.g. 9998887777 or 19998887777):
             </label>
             <input
@@ -312,34 +330,39 @@ const Refund = () => {
               name="phone"
               value={formData.phone}
               onChange={handleInputChange}
-              className="w-full p-2 border border-[#464646] rounded-[12px] bg-[#808080] focus:outline-none focus:ring-0 focus:ring-[#464646] focus:shadow-[0_0_12px_#92ae1f]"
+              className="w-full p-2 border border-gray-600 rounded-xl bg-gray-500 focus:outline-none focus:ring-0 focus:shadow-lg"
             />
           </div>
+
           <div className="mb-3">
-            <label htmlFor="comments" className="block text-[16px] font-[400] mb-4" style={{ textShadow: '0 0 0 #444444' }}>Please provide us with your mailing address and any other relevant information:
-            {errors.comments && <span className="text-red-500">{errors.comments}</span>}
+            <label htmlFor="comments" className="block text-base font-normal mb-4" style={{ textShadow: '0 0 0 #444444' }}>
+              Please provide us with your mailing address and any other relevant information:
+              {errors.comments && <span className="text-red-500">{errors.comments}</span>}
             </label>
             <textarea
               id="comments"
               name="comments"
               value={formData.comments}
               onChange={handleInputChange}
+              onBlur={handleBlur}
               rows={6}
               maxLength={500}
-              className="w-full p-3 ml-[2px] h-[166px] border border-[#464646] rounded-[12px] bg-[#808080] focus:outline-none focus:ring-0 focus:ring-[#464646] focus:shadow-[0_0_12px_#92ae1f]"
+              className="w-full p-3 ml-0.5 h-40 border border-gray-600 rounded-xl bg-gray-500 focus:outline-none focus:ring-0 focus:shadow-lg"
             />
-            <div className="text-left text-[16px]" style={{ textShadow: '0 0 0 #444444' }}>
+            <div className="text-left text-base" style={{ textShadow: '0 0 0 #444444' }}>
               {commentCharsRemaining} Characters Remaining
             </div>
           </div>
         </div>
-        <div className="mt-8 text-sm text-white text-[14px] font-[400] pl-8" style={{ textShadow: '0 0 0 #444444' }}>
+
+        <div className="mt-8 text-sm text-white pl-8" style={{ textShadow: '0 0 0 #444444' }}>
           SIID: 11900606 - JDEID: 0
         </div>
+
         <div className="mt-4 mb-4 flex justify-end">
           <button
             onClick={handleSubmit}
-            className="bg-[#FFFFFF] text-black hover:bg-[#c1f001] font-[700] rounded-[16px] px-3 py-2 text-[18px] border border-[#000] transition duration-300 ease-in-out focus:outline-none focus:ring-0 focus:ring-[#464646] focus:shadow-[0_0_12px_#92ae1f]"
+            className="bg-white text-black hover:bg-lime-300 font-bold rounded-2xl px-3 py-2 text-lg border border-black transition duration-300 ease-in-out focus:outline-none focus:ring-0 focus:shadow-lg"
           >
             Submit
           </button>
@@ -350,3 +373,4 @@ const Refund = () => {
 };
 
 export default Refund;
+
